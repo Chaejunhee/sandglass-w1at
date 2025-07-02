58c62fb3906c80847b2b2aecfc279f937102b196
Chaejunhee <144913153+Chaejunhee@users.noreply.github.com>
2025-07-02 23:12:37 +0900

    Add files via upload

---
+++ 펌웨어/sandat/config.h
@@ -0,0 +1,25 @@
+// Copyright 2024 junhee (@CJH)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define MATRiX_ROWS 5
+#define MATRIX_COLS 20
+
+#define BOOTMAGIC_ROW 0
+#define BOOTMAGIC_COLUMN 2
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ 펌웨어/sandat/keymaps/via/rules.mk
@@ -0,0 +1,3 @@
+# This file intentionally left blank
+LTO_ENABLE = yes
+VIA_ENABLE = yes
